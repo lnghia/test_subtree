e1d356c5d92a975cb92aa207d978144a7d0713b5
nghiale <trongnghialedinh@gmail.com>
2022-04-14 19:50:19 +0700

    customize UserDetails

---
+++ src/main/java/com/example/demo/CustomUserDetails/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.demo.CustomUserDetails;
+
+import com.example.demo.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        Collection<Role> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>(List.of(new SimpleGrantedAuthority("USER")));
+
+//        roles.forEach(role -> {
+//            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+//        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/demo/CustomUserDetails/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.demo.CustomUserDetails;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Repository.UserRepo;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@NoArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepo userRepo;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepo userRepo){
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepo.findByUsername(username);
+
+        if(!user.isPresent()) throw new UsernameNotFoundException(username);
+
+        return new CustomUserDetails(user.get());
+    }
+}
