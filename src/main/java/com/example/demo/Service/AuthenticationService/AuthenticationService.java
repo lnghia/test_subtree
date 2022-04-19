8eb04054f03284087bfb9c1356fc90152c4d8df7
nghiale <trongnghialedinh@gmail.com>
2022-04-19 22:09:28 +0700

    implement refreshAccessToken service

--- src/main/java/com/example/demo/Service/AuthenticationService/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Service.AuthenticationService;
-
-import com.example.demo.Entity.UserEntity;
-import com.example.demo.dto.Response.LoginResponseDTO;
-
-public interface AuthenticationService {
-    LoginResponseDTO authenticateUser(String username, String password);
-}
