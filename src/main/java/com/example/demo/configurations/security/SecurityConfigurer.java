7739ab9a8c7d6181fe58c7cc3a30becb801a9eff
nghiale <trongnghialedinh@gmail.com>
2022-04-20 22:29:33 +0700

    access denied and auth entry point exceptions

--- src/main/java/com/example/demo/configurations/security/SecurityConfigurer.java
+++ src/main/java/com/example/demo/configurations/security/SecurityConfigurer.java
@@ -1,12 +1,15 @@
 package com.example.demo.configurations.security;
 
-import com.example.demo.userdetails.CustomUserDetailsService;
+import com.example.demo.exceptions.RestAccessDeniedHandler;
+import com.example.demo.exceptions.RestAuthenticationEntryPoint;
 import com.example.demo.securityproviders.JWTAuthFilter;
+import com.example.demo.userdetails.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,12 +19,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
+
+    @Autowired
+    private RestAccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private RestAuthenticationEntryPoint authenticationEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +41,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/auth/login").permitAll()
                 .antMatchers("/api/auth/register").permitAll()
                 .antMatchers("/api/auth/refresh_tokens").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint);
 
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
