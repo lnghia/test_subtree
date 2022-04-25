193362b330a13c659caa03d3b3c6b7045f79518e
nghiale <trongnghialedinh@gmail.com>
2022-04-25 22:51:16 +0700

    unit test refreshToken()

--- src/test/java/com/example/demo/services/authentication/AuthenticationServiceImplTest.java
+++ src/test/java/com/example/demo/services/authentication/AuthenticationServiceImplTest.java
@@ -1,17 +1,14 @@
 package com.example.demo.services.authentication;
 
 
-import com.example.demo.exceptions.UserNotFoundException;
-import com.example.demo.exceptions.UsernamePasswordInvalidException;
-import com.example.demo.userdetails.CustomUserDetails;
+import com.example.demo.dto.responses.LoginResponseDTO;
 import com.example.demo.entities.UserEntity;
+import com.example.demo.exceptions.InvalidTokenException;
+import com.example.demo.exceptions.UsernamePasswordInvalidException;
 import com.example.demo.repositories.UserRepo;
 import com.example.demo.securityproviders.JWTProvider;
-import com.example.demo.dto.responses.LoginResponseDTO;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.AdditionalMatchers;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,12 +16,11 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class AuthenticationServiceImplTest {
     UserEntity initialUser;
@@ -33,8 +29,6 @@ public class AuthenticationServiceImplTest {
     PasswordEncoder passwordEncoder;
     JWTProvider jwtProvider;
     ModelMapper modelMapper;
-    LoginResponseDTO expectedLoginResponseDTO;
-    CustomUserDetails expectedCustomUserDetails;
 
     @BeforeEach
     public void beforeEach() {
@@ -43,19 +37,28 @@ public class AuthenticationServiceImplTest {
         jwtProvider = mock(JWTProvider.class);
         modelMapper = mock(ModelMapper.class);
         authService = new AuthenticationServiceImpl(userRepo, passwordEncoder, jwtProvider, modelMapper);
-        initialUser = UserEntity.builder().username("username").password("password").id(1L).build();
+        initialUser = mock(UserEntity.class);
         when(userRepo.findByUsername(anyString())).thenReturn(Optional.ofNullable(null));
         when(userRepo.findByUsername("username")).thenReturn(Optional.of(initialUser));
         when(passwordEncoder.matches(anyString(), anyString())).thenReturn(false);
         when(passwordEncoder.matches("password", initialUser.getPassword())).thenReturn(true);
         when(jwtProvider.generateAccessToken(initialUser)).thenReturn("accessToken");
         when(jwtProvider.generateRefreshToken(initialUser)).thenReturn("refreshToken");
+
+        when(jwtProvider.validateRefreshToken(anyString())).thenReturn(false);
+        when(jwtProvider.validateRefreshToken("validRefreshToken")).thenReturn(true);
+        when(jwtProvider.getUserIdFromJWT(anyString())).thenReturn(2);
+        when(jwtProvider.getUserIdFromJWT("validRefreshToken")).thenReturn(1);
+        when(userRepo.findById(anyLong())).thenReturn(Optional.ofNullable(null));
+        when(userRepo.findById(1L)).thenReturn(Optional.of(initialUser));
     }
 
     @Test
     void authenticateUser_ShouldReturnLoginResponseDTO_WhenCredentialsValid() {
         LoginResponseDTO result = authService.authenticateUser("username", "password");
 
+        verify(jwtProvider).generateAccessToken(initialUser);
+        verify(jwtProvider).generateRefreshToken(initialUser);
         assertThat(result.getRefreshToken(), is("refreshToken"));
         assertThat(result.getAccessToken(), is("accessToken"));
     }
@@ -79,5 +82,32 @@ public class AuthenticationServiceImplTest {
         assertThrows(UsernamePasswordInvalidException.class, () -> {
             authService.authenticateUser("invalidusername", "invalidpassword");
         });
+    }
+
+    @Test
+    void refreshAccessToken_ShouldReturnLoginResponseDTO_WhenRefreshTokenValid() {
+        LoginResponseDTO result = authService.refreshAccessToken("validRefreshToken");
+
+        verify(jwtProvider).generateAccessToken(initialUser);
+        verify(jwtProvider).generateRefreshToken(initialUser);
+        assertThat(result.getAccessToken(), is("accessToken"));
+        assertThat(result.getRefreshToken(), is("refreshToken"));
+    }
+
+    @Test
+    void refreshAccessToken_ShouldThrowInvalidTokenException_WhenRefreshTokenInvalid() {
+        assertThrows(InvalidTokenException.class, () -> {
+            authService.refreshAccessToken("invalidRefreshToken");
+        });
+    }
+
+    @Test
+    void shouldCreateAuthenticationServiceImpl() {
+        AuthenticationServiceImpl authenticationService = new AuthenticationServiceImpl();
+
+        assertNull(authenticationService.getJwtProvider());
+        assertNull(authenticationService.getModelMapper());
+        assertNull(authenticationService.getPasswordEncoder());
+        assertNull(authenticationService.getUserRepo());
     }
 }
